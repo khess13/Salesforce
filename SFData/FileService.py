acb532ae24bd52453e2d1cdff73dce1a18c24eb1
Kelly Hess <khess013@gmail.com>
2023-07-11 15:23:23 -0400

    refactoring

---
+++ SFData/FileService.py
@@ -0,0 +1,114 @@
+"""
+Supports recurring file operations
+"""
+import os
+import shutil
+import arrow
+
+class FileService:
+    """Provides directory clearing for dump directories"""
+    def __init__(self, file_source_dir, destination_dir):
+        self.file_source_dir = file_source_dir
+        self.data_directory = file_source_dir + "\\Data\\"
+        self.destination_dir = destination_dir
+
+    ## directory ops##
+    def get_files_from_dir(self, ext=None, altpath=None) -> list:
+        """gathers files from target directory by file type"""
+        if altpath is None:
+            temp_path = self.data_directory
+        else:
+            temp_path = altpath
+        
+        files_in_dir = os.listdir(temp_path)
+        # tilda indicates open temp file, excluding these
+        if not ext is None:
+            target_files = [temp_path + f for f in files_in_dir
+                            if ext in f and '~' not in f]
+        else:
+            target_files = [temp_path + f for f in files_in_dir
+                            if '~' not in f]
+            
+        if len(target_files) == 0:
+            raise ValueError('No files found, try checking extension')
+    
+        return target_files
+
+    def clear_destination_folder(self) -> None:
+        """clears destination folder"""
+        print('Clearing old data.')
+        try:
+            shutil.rmtree(self.destination_dir)
+            os.mkdir(self.destination_dir)
+        except Exception:
+            os.mkdir(self.destination_dir)
+
+    def copy_file(self, file_name) -> None:
+        """Copy files from data folder to destination"""
+        shutil.copyfile(self.data_directory + file_name, 
+                        self.destination_dir + file_name)
+
+    ## dependent files ##
+    def get_dependent_file_dict(self) -> dict:
+        """Gets dependent files from checkers"""
+        dep_files = self.__check_dependent_files()
+        return dep_files
+            
+    def __check_dependent_files(self) -> dict:
+        """Are all files accounted for and returns file location"""
+        # SFexport - contract.csv
+        # SFexport - extract.csv
+        # BOexport - S&D Salesforce - DTO Services.xlsx -- removing?
+        # BOexport - headcount.xlsx --- run at different time, rm?
+        # BOexport - S&D Salesforce - Scheduled.pdf
+        # BOexport - SDMap.xlsx
+        # ECCexport - EXPORT.xlsx
+        # SF map for dataloader - agencyservices.sdl
+        # SF map for dataloader - hc.sdl
+        # SF map for dataloader - contract_services.sdl
+        # SF map for dataloader - pdfimportmap.sdl
+        files_from_data = self.get_files_from_dir()
+        files_labeled_from_data = {}
+        if len(files_from_data) == 11:
+            for file in files_from_data:
+                if self.__file_date_checker(file) is False:
+                    raise Exception(f'{file} is stale')
+                if 'extract' in file:
+                    files_labeled_from_data['SFAcct'] = file
+                elif 'EXPORT' in file:
+                    files_labeled_from_data['ECCInv'] = file
+                elif 'Headcount' in file:
+                    files_labeled_from_data['HR'] = file
+                elif 'contract' in file:
+                    files_labeled_from_data['Contracts'] = file
+                elif 'Scheduled' in file:
+                    files_labeled_from_data['BOInv'] = file
+                elif 'SDMap' in file:
+                    files_labeled_from_data['SDMap'] = file
+                elif 'AgencyServices' in file:
+                    files_labeled_from_data['AgencyServices'] = file
+                elif 'agencyservices.sdl' == file:
+                    files_labeled_from_data['agyservSDL'] = file
+                elif 'hc.sdl' == file:
+                    files_labeled_from_data['hcSDL'] = file
+                elif 'contract_services.sdl' == file:
+                    files_labeled_from_data['contractSDL'] = file
+                elif 'pdfimportmap.sdl' == file:
+                    files_labeled_from_data['pdfimportSDL'] = file
+        else:
+            raise ValueError(
+                f'Check Data folder. Count is {str(len(files_from_data))}')
+        return files_labeled_from_data
+    
+    def __file_date_checker(self, sus_file) -> bool:
+        """Check staleness of file"""
+        if sus_file[-3:] == 'sdl':
+            # skip checking these files
+            return True
+        elif 'extract' in sus_file or 'SDMap' in sus_file:
+            no_of_days = arrow.now().shift(days=-200) #s/b 90
+        else:
+            no_of_days = arrow.now().shift(days=-90) #s/b 7
+        if arrow.get(os.stat(sus_file).st_mtime) > no_of_days:
+            return True
+        return False
---
+++ SFData/pdf_split.py
@@ -0,0 +1,127 @@
+"""
+Splits pdfs into individual files for upload
+Creates ContentVersion for SF Dataloader
+"""
+import os
+import re
+import getpass
+import datetime as dt
+import pandas as pd
+from tabula import read_pdf
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from FileService import FileService
+
+ROOT = os.getcwd()
+DATESTAMP = str(dt.datetime.now().strftime('%m-%d-%Y'))
+CURR_ID = getpass.getuser()
+# formerly to user desktop, avoid onedrive nagging
+OUTPUTPATH = 'C:\\Users\\'+CURR_ID+'\\PDFDrop\\'
+
+# helper functions
+def agycode_cleaner(agy_code) -> str:
+    """ because some agycodes are numbers in report"""
+    try:
+        agy_code = float(agy_code)
+        # remove .0; 0 values after decimal f = float
+        newCode = '{:0.0f}'.format(agy_code)
+        return newCode
+    except Exception:
+        return str(agy_code)
+
+
+# Load dependent files
+fs = FileService(ROOT,OUTPUTPATH)
+FS_FILE_DICT = fs.get_dependent_file_dict()
+SF_ACCT_INFO = pd.read_csv(FS_FILE_DICT.get('SFAcct'))
+
+# build dictionary because i don't know how to do this right
+SF_ACCT_INFO_DICT = {}
+for index, row in SF_ACCT_INFO.iterrows():
+    SF_ACCT_INFO_DICT[row['SCEIS_CODE__C']] = row['ID']
+
+
+# Create/clear destination folder
+fs.clear_destination_folder()
+
+# set up ContentVersion
+contentVersion = pd.DataFrame(columns=['Title',
+                                       'Description',
+                                       'VersionData',
+                                       'PathOnClient',
+                                       'FirstPublishLocationId'])
+
+# split PDF to destination
+input_pdf = PdfFileReader(open(FS_FILE_DICT.get('BOInv'), 'rb'))
+for i in range(input_pdf.numPages):
+    # moving here to clear writer
+    output = PdfFileWriter()
+    output.addPage(input_pdf.getPage(i))
+    with open(f'{OUTPUTPATH}document-page{i}.pdf', 'wb') as outputStream:
+        output.write(outputStream)
+
+# gather split files for read
+pdfs_to_parse = fs.get_files_from_dir(altpath=OUTPUTPATH)
+for loop_count, p in enumerate(pdfs_to_parse):
+    # TODO - something is wrong
+    pdfpage = read_pdf(p, pages='all')
+    df = pdfpage[0]
+    df.dropna(subset=['Total'], how='all', inplace=True)
+
+    # clean up agycode, SCI code was float
+    agycode = agycode_cleaner(df.iloc[0, 0])
+    # changes date slash to dash
+    pdate = df.iloc[0, 3].replace('/', '-')
+    # had to remove random characters like I
+    pdate = re.sub(r'[A-z]*', '', pdate)
+    # because assumed datatype was float
+    invoiceno = str(df.iloc[0, 4])[:-2]
+    invoiceamt = str(df.iloc[len(df)-1, 10])
+    # rebuild date for sort
+    tdate = '20'\
+            + pdate[-2:] + '-'\
+            + pdate[:2] + '-'\
+            + pdate[3:5]
+    gendate = DATESTAMP
+    filename = p
+
+    # outputpath = pdfdrop
+    customername = str(df.iloc[0, 2])
+    titledate = tdate + ' - ' +\
+        invoiceamt + ' - '\
+        + customername\
+        + ' - Invoice '\
+        + invoiceno +\
+        ' - Shared Services'
+    printfilename = agycode\
+        + ' Invoice Date '\
+        + pdate
+    desc = 'S&D Billing for services on '\
+        + pdate\
+        + '. Generated on '\
+        + gendate
+
+    # gets Salesforce ID for account
+    idofaccount = SF_ACCT_INFO_DICT[agycode]
+
+    # generating ContentVersion manifest
+    contentVersion.loc[loop_count] = [titledate,
+                                      desc,
+                                      filename,
+                                      filename,
+                                      idofaccount]
+    print('Logging '
+          + printfilename 
+          + ' '
+          + invoiceno 
+          + ' - '
+          + filename)
+
+print('Creating manifest for ContentVersion')
+contentVersion.to_csv(OUTPUTPATH
+                      + 'ContentVersion Generated On '
+                      + DATESTAMP
+                      + '.csv',
+                      index=False)
+fs.copy_file('pdfimportmap.sdl')
+
+print('Operation Complete!')

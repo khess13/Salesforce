8c153a32fc357c5215443571f4941380e12a4616
Kelly Hess <khess013@gmail.com>
2023-07-14 09:58:24 -0400

    init

---
+++ SFData/run.py
@@ -0,0 +1,16 @@
+"""
+Run all scripts at once. Headcount is on off day from others.
+"""
+import subprocess
+
+# TODO - would make sense to check files once here? would this make all
+# scripts need to be classes?
+
+RUN_HEADCOUNT = input('Run Headcount? y/n')
+
+if RUN_HEADCOUNT is not 'y':
+    subprocess.run(['python','xlsx_split.py'], check=False)
+    subprocess.run(['python','pdf_split.py'], check=False)
+    subprocess.run(['python','services.py'], check=False)
+else:
+    subprocess.run(['python','headcount.py'], check=False)
